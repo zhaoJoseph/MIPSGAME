83382bf9318d20dde2b6b1589b359b0972cc4f1d
zhaoJoseph <94765138+zhaoJoseph@users.noreply.github.com>
2022-02-01 17:09:36 -0500

    Update gamev1.asm

--- gamev1.asm
+++ gamev1.asm
@@ -1,37 +1,10 @@
-#####################################################################
-#
-# CSCB58 Winter 2021 Assembly Final Project
-# University of Toronto, Scarborough
-#
-# Student: Joseph Zhao, 1005913068, zhaojose
-#
+######################################################################
 # Bitmap Display Configuration:
 # -Unit width in pixels: 8 (update this as needed)
 # -Unit height in pixels: 8 (update this as needed)
 # -Display width in pixels: 256 (update this as needed)
 # -Display height in pixels: 256 (update this as needed)
 # -Base Address for Display: 0x10008000 ($gp)
-#
-# Which milestones have been reached in this submission?
-# (See the assignment handout for descriptions of the milestones)
-# Milestone 1, Mar. 28. 2021
-# Milestone 2, April, 2, 2021
-# Milestone 3, April, 2, 2021
-# Which approved features have been implemented for milestone 4?
-# (See the assignment handout for the list of additional features)
-# 1. (fill in the feature, if any)
-# 2. (fill in the feature, if any)
-# 3. (fill in the feature, if any)
-#... (add more if necessary)
-#
-# Link to video demonstration for final submission:
-# -(insert YouTube / MyMedia / other URL here). Make sure we can view it!
-#
-# Are you OK with us sharing the video with people outside course staff?
-# -no, and please share this project githublink as well!
-#
-# Any additional information that the TA needs to know:
-# -(write here, if any)
 # y position/coords means up and down rows of the bitmap 
 # x position/coords means left and right columns of the bitmap
 ######################################################################
